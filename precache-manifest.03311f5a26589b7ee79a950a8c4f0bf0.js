a3d88dcef3b599783fd71f91ea3215cf18d56bd5
hardik devani <devanibro@gmail.com>
2020-08-21 22:40:48 +0530

    Updates

---
+++ precache-manifest.03311f5a26589b7ee79a950a8c4f0bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43c929e736a906b7b6c34c96a26ebb7",
+    "url": "/reactmenu/index.html"
+  },
+  {
+    "revision": "d6b647c98a649c6e55a8",
+    "url": "/reactmenu/static/css/main.cfb5a67d.chunk.css"
+  },
+  {
+    "revision": "d3468fbaaeb94cd9d69f",
+    "url": "/reactmenu/static/js/2.b6170a3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactmenu/static/js/2.b6170a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b647c98a649c6e55a8",
+    "url": "/reactmenu/static/js/main.f143ff3c.chunk.js"
+  },
+  {
+    "revision": "48d876d4be704ab33ccc",
+    "url": "/reactmenu/static/js/runtime-main.fcb226ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactmenu/static/media/logo.5d5d9eef.svg"
+  }
+]);
